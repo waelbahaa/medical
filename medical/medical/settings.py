1ac68dfdcf379ac9f0e8ba6e948a5165b0211c44
waelbahaa <73252071+waelbahaa@users.noreply.github.com>
2023-12-08 05:55:41 -0800

    Update settings.py

--- medical/medical/settings.py
+++ medical/medical/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'medical.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pro',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'tummybed38',
+        'PORT': '3306',
     }
 }
 
